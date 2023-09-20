64860b21c3417b1cf9990586ef0a758c1a1e92eb
Bahadir BULUT <bhdrblt81@gmail.com>
2023-09-20 13:20:27 +0300

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../css/Header.css'
+
+
+export const Header = () => {
+  return (
+    <>
+    <div className="header">
+       <h3 className='mainTitle'>PisiPisi.com <br /> <span>Patili Dostların Sevilen Adresi</span> </h3>
+       
+       <div className="categoryOne">
+       <a  href="">Tüm Ürünler</a>
+       <a href="">Kedi Mamaları</a>
+       <a href="">Kedi Kumları</a>
+       </div>
+        </div>
+     </>
+    
+  )
+}
+
