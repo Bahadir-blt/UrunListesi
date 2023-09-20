982513089f6235f2facf178d69d7f396af157e6d
Bahadir BULUT <bhdrblt81@gmail.com>
2023-09-20 19:53:15 +0300

    data oluşturuldu

---
+++ src/productData.js
@@ -0,0 +1,23 @@
+const productData = [
+    {
+        id: 1,
+        name: 'HILLS ADULT Kedi Maması 1500 gr',
+        price: '300 ₺',
+        stock: 25 ,
+        image: 'src/images/Hills Kedi Maması.jpeg' ,
+    },
+    {
+        id: 2,
+        name: 'ROYAL CANIN Kedi Maması',
+        price: '228 ₺',
+        stock: 5 ,
+        image: 'src/images/Royal Canın.jpeg' ,
+    },
+    {
+        id: 3,
+        name: 'ProPlan Kedi Maması',
+        price: '424 ₺',
+        stock: 3 ,
+        image: 'src/images/Pro Plan.jpeg' ,
+    }
+]
