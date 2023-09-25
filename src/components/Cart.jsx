34204df8d6906daf9020564da321d431cbdec689
Bahadir BULUT <bhdrblt81@gmail.com>
2023-09-25 12:44:24 +0300

    toplama işlemindeki hatalar giderildi

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,7 +3,7 @@ import '../css/cart.css'
 
 
 const Cart = ({ cart, emptyCart }) => {
-const total = cart.reduce ((cart, item) => cart+ item.price, 0) ;
+const total = cart.reduce ((cart, item) => cart + item.price, 0) ;
 
 
     if (cart.length === 0 ) return;
@@ -14,14 +14,14 @@ const total = cart.reduce ((cart, item) => cart+ item.price, 0) ;
                 <h2 className='Basket'>Sepet</h2>
                 <ul>
                     {cart.map((item) => (
-                    <li className='SpanTwo'>
+                    <li className='SpanTwo' key={item.id}>
                         <span> {item.name} </span>
                         <span> {item.price} </span>
                     </li>
                     ))}
                 </ul>
                 <hr className='hr' />
-                <p className='totalPrice'>Toplam: {total} </p>
+                <p className='totalPrice'>Toplam: {total} TL </p>
                 <button className='emptyCart'onClick={emptyCart} >Sepeti Boşalt</button>
             </div>
             </>
--- src/productData.js
+++ src/productData.js
@@ -2,47 +2,47 @@ const productData = [
     {
         id: 1,
         name: 'HILLS Kısırlaştırılmış Ördekli Kedi Maması',
-        price: '300 TL',
-        stock: 'Güncel Stok Durumu 1' ,
-        image: 'src/images/Hills Kedi Maması.jpeg' ,
+        price: 300,
+        stock: 'Güncel Stok Durumu 1',
+        image: 'src/images/Hills Kedi Maması.jpeg',
     },
     {
         id: 2,
         name: 'ROYAL CANIN Kitten 26 " Yavru Kedi Maması',
-        price: '228 TL',
-        stock:  'Güncel Stok Durumu 5' ,
-        image: 'src/images/Royal Canın.jpeg' ,
+        price: 228,
+        stock: 'Güncel Stok Durumu 5',
+        image: 'src/images/Royal Canın.jpeg',
 
     },
-    {   
+    {
         id: 3,
         name: 'ProPlan True Nature Kısırlaştırlmış Kedi Maması',
-        price: '768 TL',
-        stock: 'Güncel Stok Durumu 3' ,
-        image: 'src/images/Pro Plan.jpeg' ,
+        price: 768,
+        stock: 'Güncel Stok Durumu 3',
+        image: 'src/images/Pro Plan.jpeg',
     },
     {
         id: 4,
         name: 'Ever Clean Topaksız  Kokusuz  Kedi Kumu',
-        price: '129 TL',
-        stock:  'Güncel Stok Durumu 50' ,
-        image: 'src/images/Ever Clean.jpeg' ,
+        price: 129,
+        stock: 'Güncel Stok Durumu 50',
+        image: 'src/images/Ever Clean.jpeg',
 
     },
     {
         id: 5,
         name: 'Proline Karbonlu Kedi Kumu İnce Taneli',
-        price: '228 TL',
-        stock:  'Güncel Stok Durumu 50' ,
-        image: 'src/images/proline-karbonlu-kedi-kumu.jpg' ,
+        price: 228 ,
+        stock: 'Güncel Stok Durumu 50',
+        image: 'src/images/proline-karbonlu-kedi-kumu.jpg',
 
     },
     {
         id: 6,
         name: 'VanCat Kokusuz Renksiz Bentonite Kedi Kumu',
-        price: '118 TL',
-        stock:  'Güncel Stok Durumu 5' ,
-        image: 'src/images/vancat.jpg' ,
+        price: 118,
+        stock: 'Güncel Stok Durumu 5',
+        image: 'src/images/vancat.jpg',
 
     },
 ];
